e953a584638b29d0f0544027c3b2dc69fd2b401a
Paul Negedu <37233089+paulnegz@users.noreply.github.com>
2023-06-18 01:12:13 -0500

    Update util.py

--- util.py
+++ util.py
@@ -2,6 +2,14 @@ from functools import wraps
 import time
 from random import randint
 
+
+get_ordered_list = lambda WIDTH: [x for x in range(WIDTH)]
+get_random_list = lambda WIDTH: [randint(0,WIDTH) for _ in range(WIDTH)]
+
+def get_repeated_list(WIDTH: int)->list:
+    repeated_value = randint(0,WIDTH)
+    return [repeated_value for _ in range(WIDTH)]
+
 def timeit(func):
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):
@@ -13,11 +21,3 @@ def timeit(func):
         print(f'Function {func.__name__}\t took {total_time:.4f} seconds')
         return result
     return timeit_wrapper
-
-
-def get_repeated_list(WIDTH: int)->list:
-    repeated_value = randint(0,WIDTH)
-    return [repeated_value for _ in range(WIDTH)]
-
-get_random_list = lambda WIDTH: [randint(0,WIDTH) for _ in range(WIDTH)]
-get_ordered_list = lambda WIDTH: [x for x in range(WIDTH)]
