5e95c43ed63ed776a6478d2dbe283280e773517a
Paul Negedu <paul.negedu@yahoo.com>
2023-06-19 13:34:50 -0500

    main file

---
+++ main.py
@@ -0,0 +1,23 @@
+from util import get_random_list
+from copy import deepcopy
+from sort import *
+
+
+if __name__ == "__main__":
+    WIDTH = 4000
+    random_list = get_random_list(WIDTH)
+    print(f"Sorting list with {WIDTH} elements...")
+    bubble_result = bubble_sort(deepcopy(random_list))
+    selection_result = selection_sort(deepcopy(random_list))
+    insertion_result = insertion_sort(deepcopy(random_list))
+    merge_result = merge_sort(deepcopy(random_list))
+    quick_result = quick_sort(deepcopy(random_list))
+    radix_result = radix_sort(deepcopy(random_list))
+    tim_result = tim_sort(deepcopy(random_list))
+    heap_result = heap_sort(deepcopy(random_list))
+    python_result = python_sort(deepcopy(random_list))
+
+    assert bubble_result == selection_result == insertion_result
+    assert insertion_result == merge_result == quick_result
+    assert quick_result == radix_result == tim_result
+    assert tim_result == heap_result == python_result
