554732c08e70b629e5faeb81108789dff6d12c26
Paul Negedu <paul.negedu@yahoo.com>
2023-06-19 16:51:30 -0500

    tim_sort

--- sort.py
+++ sort.py
@@ -34,8 +34,6 @@ def merge_sort(unordered_list :list)->list:
 @timeit
 def tim_sort(unordered_list :list)->list:
     run_size = 64
-    if len(unordered_list)<run_size:
-        run_size=len(unordered_list)
     run_num = ceil(len(unordered_list)/run_size)
     bucket_outter=[[] for _ in range(run_num)]
     for index in range(run_num):
@@ -47,7 +45,7 @@ def tim_sort(unordered_list :list)->list:
 
 @timeit
 def tim_sort_new(unordered_list :list)->list:
-    return tim_sort_rec(unordered_list)
+    return tim_sort_new_rec(unordered_list)
 
 def tim_sort_new_rec(unordered_list :list)->list:
     run_size = 64
