85e14f10d581ae2a0669cce72db97b1937b68f20
Paul Negedu <paul.negedu@yahoo.com>
2023-06-19 21:54:24 -0500

    README

--- sort.py
+++ sort.py
@@ -7,52 +7,52 @@ from util import timeit, merge
 RUN_SIZE = 128
 
 @timeit
-def quick_sort(unordered_list :list)->list:
-    def _quick_sort(unordered_list :list)->list:
-        if len(unordered_list)<=1: return unordered_list
+def quick_sort(array :list)->list:
+    def _quick_sort(array :list)->list:
+        if len(array)<=1: return array
         
-        pivot = unordered_list.pop()
+        pivot = array.pop()
         is_bigger = lambda x: x>pivot
-        left_list = [x for x in unordered_list if not is_bigger(x)]
-        right_list = [x for x in unordered_list if is_bigger(x)]
+        left_list = [x for x in array if not is_bigger(x)]
+        right_list = [x for x in array if is_bigger(x)]
         left, right = _quick_sort(left_list), _quick_sort(right_list)
         return [*left, pivot, *right]
-    return _quick_sort(unordered_list)
+    return _quick_sort(array)
 
 
 @timeit
-def merge_sort(unordered_list :list)->list:
-    def _merge_sort(unordered_list :list)->list:
-        length = len(unordered_list)
-        if length <= 1: return unordered_list
+def merge_sort(array :list)->list:
+    def _merge_sort(array :list)->list:
+        length = len(array)
+        if length <= 1: return array
         
         mid = length//2
-        left = _merge_sort(unordered_list[:mid])
-        right = _merge_sort(unordered_list[mid:])
+        left = _merge_sort(array[:mid])
+        right = _merge_sort(array[mid:])
         return merge(left,right)
-    return _merge_sort(unordered_list)
+    return _merge_sort(array)
 
 
 @timeit
-def tim_sort(unordered_list :list)->list:
-    run_num = ceil(len(unordered_list)/RUN_SIZE)
+def tim_sort(array :list)->list:
+    run_num = ceil(len(array)/RUN_SIZE)
     buckets = [[] for _ in range(run_num)]
     for index in range(run_num):
         start, stop = RUN_SIZE*index, RUN_SIZE*(index+1) 
-        buckets[index] = unordered_list[start:stop]
+        buckets[index] = array[start:stop]
     bucket = (_insertion_sort(inner) for inner in buckets)
     return merge(*bucket) 
 
 
 @timeit
-def tim_sort_recursive(unordered_list :list)->list:
-    return _tim_sort_recursive(unordered_list)
+def tim_sort_recursive(array :list)->list:
+    return _tim_sort_recursive(array)
 
-def _tim_sort_recursive(unordered_list :list)->list:
-    left = _insertion_sort(unordered_list[:RUN_SIZE]) 
-    if len(unordered_list)<RUN_SIZE: return left
+def _tim_sort_recursive(array :list)->list:
+    left = _insertion_sort(array[:RUN_SIZE]) 
+    if len(array)<RUN_SIZE: return left
 
-    right = _tim_sort_recursive(unordered_list[RUN_SIZE:])
+    right = _tim_sort_recursive(array[RUN_SIZE:])
     return merge(left, right)
 
 
@@ -66,48 +66,48 @@ def heap_sort(random_list: list)->list:
 
 
 @timeit
-def bubble_sort(unordered_list :list)->list:
-    for y in range(1, len(unordered_list)):
+def bubble_sort(array :list)->list:
+    for y in range(1, len(array)):
         swapped: bool = False
-        for x in range(1,len(unordered_list)-(y-1)):
-            if unordered_list[x]<unordered_list[x-1]:
-                unordered_list[x], unordered_list[x-1], swapped = unordered_list[x-1], unordered_list[x], True
+        for x in range(1,len(array)-(y-1)):
+            if array[x]<array[x-1]:
+                array[x], array[x-1], swapped = array[x-1], array[x], True
         if not swapped: break
-    return unordered_list
+    return array
 
 
 @timeit
-def insertion_sort(unordered_list :list)->list:
-    return _insertion_sort(unordered_list)
+def insertion_sort(array :list)->list:
+    return _insertion_sort(array)
 
-def _insertion_sort(unordered_list :list)->list:
-    for index in range(1,len(unordered_list)):
+def _insertion_sort(array :list)->list:
+    for index in range(1,len(array)):
         behind=index-1
-        while behind>=0 and unordered_list[behind] > unordered_list[behind+1]:
-            unordered_list[behind], unordered_list[behind+1] = unordered_list[behind+1], unordered_list[behind]
+        while behind>=0 and array[behind] > array[behind+1]:
+            array[behind], array[behind+1] = array[behind+1], array[behind]
             behind-=1
-    return unordered_list
+    return array
 
 
 @timeit
-def selection_sort(unordered_list :list)->list:
-    for index_y, _ in enumerate(unordered_list):
+def selection_sort(array :list)->list:
+    for index_y, _ in enumerate(array):
         min_index = index_y
-        for index in range(index_y,len(unordered_list)):
-            if unordered_list[index] < unordered_list[min_index]: min_index = index
-        unordered_list[min_index], unordered_list[index_y] = unordered_list[index_y], unordered_list[min_index]
-    return unordered_list
+        for index in range(index_y,len(array)):
+            if array[index] < array[min_index]: min_index = index
+        array[min_index], array[index_y] = array[index_y], array[min_index]
+    return array
 
 
 @timeit
-def radix_sort(unordered_list :list)->list:
-    base, max_length = 10, len(str(max(unordered_list)))
+def radix_sort(array :list)->list:
+    base, max_length = 10, len(str(max(array)))
     for n in range(max_length):
         bucket=[[] for _ in range(base)]
-        for item in unordered_list:
+        for item in array:
             bucket[item//base**n %base].append(item)
-        unordered_list = reduce(lambda acc, current: [*acc,*current], bucket)
-    return unordered_list
+        array = reduce(lambda acc, current: [*acc,*current], bucket)
+    return array
 
 
 @timeit
